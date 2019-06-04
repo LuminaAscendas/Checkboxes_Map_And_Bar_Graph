618626246bc2f40dc5345b5ac4521d3903c9a495
rajarajan <rajarajan.rajendran@luminad.com>
2019-06-04 19:25:22 +0530

    First Commit

---
+++ assets/js/data.js
@@ -0,0 +1,7 @@
+var direction_text='<span id="dir_span">Directions:</span> Click on the location of coal deposits in the key and compare their distribution in the graph.';
+var checkBox = 6;
+var color=['pink','brown','aqua','violet','neavyblue','yellow'];
+var label=['Asia and Oceania','Eastern Europe and Russia','North America','Western Europe','Africa','Central and South America'];
+
+
+thumb_label=['To select full size image of this thumbnail and corresponding details, press the enter or spacebar key. To move to next thumbnail, press the tab key.']
